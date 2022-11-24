38d1b8330767100cb9e9584a21d58d44f731bee9
Oscar Forero <o.forero@icloud.com>
2022-11-24 20:39:09 +0100

    Use local import for the provider package

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	kopicloud "gitlab.com/KopiCloud/kopicloud-ad-tf-provider/kopicloud"
+	kopicloud "kopicloud/kopicloud"
 )
 
 func main() {
