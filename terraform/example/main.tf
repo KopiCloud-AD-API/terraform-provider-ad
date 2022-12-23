9754454fc4afeba97b83158c4e75a5a5dc1a83ec
Oscar Forero <o.forero@icloud.com>
2022-12-23 11:10:09 +0100

    Add support for Data Sources with terraform parameters

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -11,34 +11,46 @@ provider "kopicloud" {
   token   = "Basic b3NjYXI6UzFsdjNyQnVsbDN0"
 }
 
-data "kopicloud_all_computers" "test" {}
+data "kopicloud_computers" "test" {
+  oupath= "OU=Domain Controllers,DC=kopicloud,DC=local"
+  recursive=true
+}
 
 # Returns all computers
-output "all_computers_list" {
+output "computers_list" {
   description = "Existing computers"
-  value = data.kopicloud_all_computers.test
+  value = data.kopicloud_computers.test
 }
 
-data "kopicloud_all_ous" "test" {}
 
-# Returns all ous
-output "all_ous_list" {
-  description = "Existing OUs"
-  value = data.kopicloud_all_ous.test
-}
+# data "kopicloud_all_computers" "test" {}
 
-data "kopicloud_all_dns_a_records" "test" {}
+# # Returns all computers
+# output "all_computers_list" {
+#   description = "Existing computers"
+#   value = data.kopicloud_all_computers.test
+# }
 
-# Returns all DNS A Records
-output "all_dns_a_records" {
-  description = "Existing DNS A Records"
-  value = data.kopicloud_all_dns_a_records.test
-}
+# data "kopicloud_all_ous" "test" {}
 
-data "kopicloud_all_dns_aaaa_records" "test" {}
+# # Returns all ous
+# output "all_ous_list" {
+#   description = "Existing OUs"
+#   value = data.kopicloud_all_ous.test
+# }
 
-# Returns all DNS AAAA Records
-output "all_dns_aaaa_records" {
-  description = "Existing DNS AAAA Records"
-  value = data.kopicloud_all_dns_aaaa_records.test
-}
+# data "kopicloud_all_dns_a_records" "test" {}
+
+# # Returns all DNS A Records
+# output "all_dns_a_records" {
+#   description = "Existing DNS A Records"
+#   value = data.kopicloud_all_dns_a_records.test
+# }
+
+# data "kopicloud_all_dns_aaaa_records" "test" {}
+
+# # Returns all DNS AAAA Records
+# output "all_dns_aaaa_records" {
+#   description = "Existing DNS AAAA Records"
+#   value = data.kopicloud_all_dns_aaaa_records.test
+# }
