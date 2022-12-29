48f39590261b319a0ad0726dbe7c8e133f186524
Oscar Forero <o.forero@icloud.com>
2022-12-30 00:15:35 +0100

    FIx the name of the code generator tool

--- terraform/main.go
+++ terraform/main.go
@@ -9,8 +9,8 @@ import (
 
 //go:generate oapi-codegen -package=api   -generate=types   -o api/kopicloud.types.gen.go https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
 //go:generate oapi-codegen -package=api   -generate=client  -o api/kopicloud.client.gen.go https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
-//go:generate openapi-terraform-generator -api=kopicloud/api -package=provider -generate=schemas -o provider/kopicloud.schemas.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
-//go:generate openapi-terraform-generator -api=kopicloud/api -package=provider -generate=converters -o provider/kopicloud.converters.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
+//go:generate openapi-terraform-provider-generator -api=kopicloud/api -package=provider -generate=schemas -o provider/kopicloud.schemas.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
+//go:generate openapi-terraform-provider-generator -api=kopicloud/api -package=provider -generate=converters -o provider/kopicloud.converters.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
 //go:generate ../bin/kopicloud-ad-tf-provider-generator -i generator-inputs/datasources.json -o provider/datasources.gen.go -generate datasources
 //go:generate ../bin/kopicloud-ad-tf-provider-generator -i generator-inputs/resources.json -o provider/resources.gen.go -generate resources
 
