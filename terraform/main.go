7b93d3619eef8d89a9678afa6bf62b957e26a83a
Oscar Forero <o.forero@icloud.com>
2022-12-30 11:03:06 +0100

    Improve the code to work in a general location by using package names witth gitlab URL

--- terraform/main.go
+++ terraform/main.go
@@ -4,13 +4,13 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	kcp "kopicloud/provider"
+	kcp "gitlab.com/KopiCloud/kopicloud-ad-tf-provider/provider"
 )
 
 //go:generate oapi-codegen -package=api   -generate=types   -o api/kopicloud.types.gen.go https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
 //go:generate oapi-codegen -package=api   -generate=client  -o api/kopicloud.client.gen.go https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
-//go:generate openapi-terraform-provider-generator -api=kopicloud/api -package=provider -generate=schemas -o provider/kopicloud.schemas.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
-//go:generate openapi-terraform-provider-generator -api=kopicloud/api -package=provider -generate=converters -o provider/kopicloud.converters.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
+//go:generate openapi-terraform-provider-generator -api=gitlab.com/KopiCloud/kopicloud-ad-tf-provider/api -package=provider -generate=schemas -o provider/kopicloud.schemas.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
+//go:generate openapi-terraform-provider-generator -api=gitlab.com/KopiCloud/kopicloud-ad-tf-provider/api -package=provider -generate=converters -o provider/kopicloud.converters.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
 //go:generate ../bin/kopicloud-ad-tf-provider-generator -i generator-inputs/datasources.json -o provider/datasources.gen.go -generate datasources
 //go:generate ../bin/kopicloud-ad-tf-provider-generator -i generator-inputs/resources.json -o provider/resources.gen.go -generate resources
 
--- terraform/provider/provider.go
+++ terraform/provider/provider.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-	kcapi "kopicloud/api"
+	kcapi "gitlab.com/KopiCloud/kopicloud-ad-tf-provider/api"
 )
 
 // Provider -
