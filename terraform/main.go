221e9be3075aa3e14f5277a82ebee62dbd46a027
Oscar Forero <o.forero@icloud.com>
2023-02-03 17:50:52 +0100

    Adding support for dastasources with new API schemas

--- terraform/main.go
+++ terraform/main.go
@@ -9,9 +9,9 @@ import (
 
 //go:generate oapi-codegen -package=api   -generate=types   -o api/kopicloud.types.gen.go https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
 //go:generate oapi-codegen -package=api   -generate=client  -o api/kopicloud.client.gen.go https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
-//go:generate openapi-terraform-provider-generator -api=gitlab.com/KopiCloud/kopicloud-ad-tf-provider/api -package=provider -generate=schemas -o provider/kopicloud.schemas.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
+//go:generate openapi-terraform-provider-generator -api=gitlab.com/KopiCloud/kopicloud-ad-tf-provider/api -package=provider -include computer -generate=schemas -o provider/kopicloud.schemas.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
 //go:generate openapi-terraform-provider-generator -api=gitlab.com/KopiCloud/kopicloud-ad-tf-provider/api -package=provider -generate=converters -o provider/kopicloud.converters.gen.go -url https://labapi.kopicloud-ad-api.com/swagger/v1/swagger.json
-// :generate ../bin/kopicloud-ad-tf-provider-generator -i generator-inputs/datasources.json -o provider/datasources.gen.go -generate datasources
+//go:generate ../bin/kopicloud-ad-tf-provider-generator -i generator-inputs/datasources.json -o provider/datasources.gen.go -generate datasources
 // :generate ../bin/kopicloud-ad-tf-provider-generator -i generator-inputs/dns-records.json,generator-inputs/dns-zones.json -o provider/resources-schema.gen.go -generate resources-schema
 // :generate ../bin/kopicloud-ad-tf-provider-generator -i generator-inputs/dns-records.json -o provider/resources-dns-records.gen.go -generate resource-crud
 // :generate ../bin/kopicloud-ad-tf-provider-generator -i generator-inputs/dns-zones.json -o provider/resources-dns-zones.gen.go -generate resource-crud
